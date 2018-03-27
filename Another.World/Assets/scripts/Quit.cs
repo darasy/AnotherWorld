300959b8ed90d8324f13870c3b97ac41a17b7f8c
Ira Chan <irachan@buffalo.edu>
2018-03-27 13:24:47 -0400

    quit button

---
+++ Another.World/Assets/scripts/Quit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Quit : MonoBehaviour
+{
+    void Start() { }
+    void Update() { }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
