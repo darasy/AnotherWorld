d9170db0f62f963b9046a8e528c83e3d811a1a81
Ira Chan <irachan@buffalo.edu>
2018-03-28 15:16:15 -0400

    place objects
    actually end me

---
+++ Another.World/Assets/scripts/itemSelect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemSelect : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () {
+		
+//	}
+	
+	// Update is called once per frame
+
+	void Update () {
+		
+	}
+    public int assetNum = 0;
+    //public string assetName;
+       IEnumerator Start()
+    {
+        string url = "http://ec2-18-232-184-23.compute-1.amazonaws.com/assetbundles/asset_bundle_1";
+        WWW www = new WWW(url);
+        while (!www.isDone)
+        {
+            
+            if(www.assetBundle!=null)
+            {
+                AssetBundle bundle = www.assetBundle;
+                string[] assetList = bundle.GetAllAssetNames();
+                GameObject asset = bundle.LoadAsset(assetList[assetNum]) as GameObject;
+                Instantiate(asset);
+            }        }
+        yield return www;
+    }
+   /* public void load1()
+    {
+        string url = "http://ec2-18-232-184-23.compute-1.amazonaws.com/assetbundles/asset_bundle_1";
+        WWW www = new WWW(url);
+        while (www.isDone)
+        {
+
+            if (www.assetBundle != null)
+            {
+                AssetBundle bundle = www.assetBundle;
+                string[] assetList = bundle.GetAllAssetNames();
+                GameObject asset = bundle.LoadAsset(assetList[1]) as GameObject;
+                Instantiate(asset);
+            }
+        }
+        yield return www;*/
+     
+}
---
+++ Another.World/Assets/tempItemPlacement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempItemPlacement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void pressed()
+    {
+        GameObject objPlaced = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        objPlaced.transform.position = this.transform.position;
+        Instantiate(objPlaced);
+    }
+    public void cube()
+    {
+        GameObject objPlaced = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        objPlaced.transform.position = this.transform.position;
+        Instantiate(objPlaced);
+    }
+    public void capsule()
+    {
+        GameObject objPlaced = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        objPlaced.transform.position = this.transform.position;
+        Instantiate(objPlaced);
+    }
+    public void plane()
+    {
+        GameObject objPlaced = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        objPlaced.transform.position = this.transform.position;
+        Instantiate(objPlaced);
+    }
+    public void cylinder()
+    {
+        GameObject objPlaced = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        objPlaced.transform.position = this.transform.position;
+        Instantiate(objPlaced);
+    }
+}
